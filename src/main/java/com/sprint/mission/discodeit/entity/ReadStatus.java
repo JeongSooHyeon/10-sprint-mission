590c8d79157d1e7f6137ff056d512cefcf71b785
JeongSooHyeon <jsh75721@naver.com>
2026-02-24 14:10:57 +0900

    fix : readstatus lastReadAt 기본값 Instant.EPOCH로 변경

--- src/main/java/com/sprint/mission/discodeit/entity/ReadStatus.java
+++ src/main/java/com/sprint/mission/discodeit/entity/ReadStatus.java
@@ -6,24 +6,25 @@ import java.time.Instant;
 import java.util.UUID;
 
 @Getter
-public class ReadStatus extends BaseEntity{
-    private final UUID userId;
-    private final UUID channelId;
-    private Instant lastReadAt;
+public class ReadStatus extends BaseEntity {
 
-    public ReadStatus(UUID userId, UUID channelId) {
-        super(UUID.randomUUID(), Instant.now());
-        this.userId = userId;
-        this.channelId = channelId;
-        lastReadAt = Instant.MIN;
-    }
+  private final UUID userId;
+  private final UUID channelId;
+  private Instant lastReadAt;
 
-    public void updateLastReadAt(Instant lastReadAt){
-        this.lastReadAt = lastReadAt;
-        this.onUpdate();
-    }
+  public ReadStatus(UUID userId, UUID channelId) {
+    super(UUID.randomUUID(), Instant.now());
+    this.userId = userId;
+    this.channelId = channelId;
+    lastReadAt = Instant.EPOCH;
+  }
 
-    public boolean isUnread(Instant messageCreatedAt){
-        return lastReadAt.isBefore(messageCreatedAt);
-    }
+  public void updateLastReadAt(Instant lastReadAt) {
+    this.lastReadAt = lastReadAt;
+    this.onUpdate();
+  }
+
+  public boolean isUnread(Instant messageCreatedAt) {
+    return lastReadAt.isBefore(messageCreatedAt);
+  }
 }
